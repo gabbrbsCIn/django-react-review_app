e287dca7b25e2d654082abe911db42aaede039ed
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-04-04 17:40:41 -0300

    feat (frontend): criada rota para proteger telas

---
+++ frontend/src/api.js
@@ -0,0 +1,25 @@
+import axios from axios;
+import { ACCESS_TOKEN } from "./constants";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+
+export default api;
---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,56 @@
+import { Navigate } from 'react-router-dom';
+import { jwtdecode } from 'jwt-decode';
+import api from '../api';
+import { REFRESH_TOKEN, ACCESS_TOKEN } from '../constants';
+import { useState, useEffect } from 'react';
+
+
+function ProtectedRoute(children) {
+    const [isAuthorized, setIsAuthorized] = useState(null);
+
+    useEffect(() => {
+        auth().catch(()=> setIsAuthorized(false));
+    }, []);
+
+
+    const refreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+        try {
+            const response = await api.post('/auth/refresh', {
+                refresh: refreshToken,
+            });
+            if (response.status === 200) {
+                localStorage.setItem(ACCESS_TOKEN, response.data.access);
+                setIsAuthorized(true);
+            } else {
+                setIsAuthorized(false);
+            }
+        } catch (error) {
+            setIsAuthorized(false);
+        }
+
+        const auth = async () => {
+            const token = localStorage.getItem(ACCESS_TOKEN);
+            if (!token) {
+                setIsAuthorized(false);
+                return;
+            }
+
+            const decoded = jwtdecode(token);
+            const tokenExp = decoded.exp;
+            const now = Date.now() / 1000;
+
+            if (tokenExp < now) {
+                await refreshToken();
+            } else {
+                setIsAuthorized(true);
+            }
+            if ((isAuthorized) === null) {
+                return <div>Loading...</div>;
+            }
+
+            return isAuthorized ? children : <Navigate to="/login" />;
+        }
+    }
+
+}
