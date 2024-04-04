82c1f0707e5493a3c36b9b1e77ad81e95b3e8456
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-04-04 14:38:05 -0300

    refactor: atualizado serializer, views, e arquivos de configuração

--- backend/backend/app/views.py
+++ backend/backend/app/views.py
@@ -7,5 +7,5 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = User 
+    serializer_class = UserSerializer
     permission_classes = [AllowAny]
--- backend/backend/backend/urls.py
+++ backend/backend/backend/urls.py
@@ -6,7 +6,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/user/register/', CreateUserView.as_view(), name='register'),
-    path('app/toker/', TokenObtainPairView.as_view(), name='get_token'),
-    path('app/toker/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('app/token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('app/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
     path('app-auth/', include("rest_framework.urls")),
+    path('app/', include("app.urls")),
 ]
