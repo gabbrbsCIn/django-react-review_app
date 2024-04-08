c64799b1849efd3ca89a386b361a6db46ee62d37
Gabriel <gbgo@cin.ufpe.br>
2024-04-08 12:47:11 -0300

    feat(frontend): criado componente do formulário

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
-function Login(){
-    return <div>Login</div>
+import Form from "../components/Form";
 
+
+function Login(){
+    return <Form route="/api/token/" method="login" />
 }
 
 export default Login;
