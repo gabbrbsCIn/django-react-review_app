89588c43b37f4be72e4b531c2ad541c4a625b0cb
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-04-09 11:55:32 -0300

    feat(frontend): adicionada estilização às páginas de Login e Register

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api";
 import { ACCESS_TOKEN, ACCESS_REFRESH } from "../constants";
-
+import "../index.css";
 
 function Form({ route, method }) {
 
@@ -39,24 +39,32 @@ function Form({ route, method }) {
         }
     }
     return (
-        <form onSubmit={handleSubmit} className="form-container">
-            <h1> {name} </h1>
-            <input
-                className="form-input"
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Nome de Usuário"
-            />
-            <input
-                className="form-input"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Senha"
-            />
-            <button className="form-button" type="submit" > {name} </button>
-        </form>
+        <>
+            <div className="relative w-screen h-screen font-poppins bg-amber-400 flex items-center justify-center">
+                <div className="absolute inset-0 z-0 " style={{ backgroundImage: "url('/2174692.svg')", backgroundSize: "cover", opacity: "0.1" }}></div>
+                <div className="bg-amber-200 py-10 px-6 rounded-2xl shadow-2xl z-10">
+                    <form onSubmit={handleSubmit} className="flex flex-col items-center space-y-11 mb-5 pr-4 pl-4">
+                        <h1 className="text-3xl"> {name} </h1>
+                        <input
+                            className="appearance-none border border-gray-300 rounded-2xl w-full py-2 px-3 mb-3 mr-4 ml-4 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300 ease-in-out"
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            placeholder="Nome de Usuário"
+                        />
+                        <input
+                            className="appearance-none border border-gray-300 rounded-2xl w-full py-2 px-3 mb-3 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300 ease-in-out"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Senha"
+                        />
+                        <button className="bg-blue-600 hover:bg-blue-800 text-white py-2 px-6 rounded-full shadow-lg transition-all duration-300 ease-in-out" type="submit"> {name} </button>
+                    </form>
+                </div>
+            </div>
+
+        </>
     )
 
 }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"]
+      },
+      colors: {
+        "yl-400": "#FFE073",
+        "yl-500": "#FFD234",
+      },
+    },
+  },
+  plugins: [],
+}
+
