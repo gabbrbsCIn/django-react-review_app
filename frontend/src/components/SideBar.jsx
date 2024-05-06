0475138e94bd9d58e40f4ad8aea613bcb35218cd
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-05-06 11:27:37 -0300

    feat: feita estilização da sidebar

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -37,7 +37,7 @@ export default function Sidebar({ children }) {
 
     return (
         <aside className="h-screen">
-            <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+            <nav className="h-full flex flex-col border-r border-gray-600 bg-amber-300 shadow-sm">
                 <div className="p-4 pb-2 flex justify-between items-center">
                     <img
                         src=""
@@ -48,7 +48,7 @@ export default function Sidebar({ children }) {
                         onClick={() => setExpanded((curr) => !curr)}
                         className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
                     >
-                        {expanded ? <ChevronFirst /> : <ChevronLast />}
+                        {expanded ? <ChevronFirst color="#111827" /> : <ChevronLast color="#111827" />}
                     </button>
                 </div>
 
@@ -56,14 +56,14 @@ export default function Sidebar({ children }) {
                     <ul className="flex-1 px-3">{children}</ul>
                 </SidebarContext.Provider>
 
-                <div className="border-t flex p-3 w-full">
+                <div className="border-t border-gray-600 flex p-3 w-full">
 
                     <div className={`flex justify-between items-center overflow-hidden transition-all ${expanded ? "w-60 ml-3" : "w-0"}`}>
-                        <div className="leading-4">
-                            {isLoading ? null : <h4 className="font-semibold">Olá, {username}!</h4>}
+                        <div className="leading-4"> 
+                            {isLoading ? null : <h4 className="flex p-2 rounded-md font-semibold bg-gray-800 text-white">  {username}</h4>}
                         </div>
-                        <button className="p-1.5 rounded-lg bg-gray-100 hover:bg-gray-200 ">
-                            <LogOut size={24} onClick={handleLogout} />
+                        <button className={"p-1 rounded-md bg-transparent transition-colors duration-300 hover:bg-red-500" } >
+                            <LogOut color="#fff"  onClick={handleLogout}  />
                         </button>
 
                     </div>
@@ -84,20 +84,20 @@ export function SidebarItem({ icon, text, active, alert }) {
         transition-colors group
         ${active
                     ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
-                    : "hover:bg-indigo-50 text-gray-600"
+                    : "hover:bg-gray-800 text-gray-600"
                 }
     `}
         >
             {icon}
             <span
-                className={`overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"
+                className={`overflow-hidden transition-all text-white ${expanded ? "w-52 ml-3" : "w-0"
                     }`}
             >
                 {text}
             </span>
             {alert && (
                 <div
-                    className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? "" : "top-2"
+                    className={`absolute right-2 w-2 h-2 rounded bg-indigo  -400 ${expanded ? "" : "top-2"
                         }`}
                 />
             )}
