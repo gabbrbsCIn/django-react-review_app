6f662907ba2cde62844ebdd40a0f7f64f047af40
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-05-09 14:49:14 -0300

    feat: adicionado roteamento dos componentes da sidebar

--- frontend/src/components/SideBarItems.jsx
+++ frontend/src/components/SideBarItems.jsx
@@ -1,12 +1,20 @@
 import { SidebarItem } from "./SideBar";
 import { LayoutDashboard, BookTypeIcon, ClipboardCheckIcon } from "lucide-react";
+import { Link } from "react-router-dom";
+
 
 function SideBarItems() {
     return (
         <>
-            <SidebarItem icon={<LayoutDashboard color="#1f2937" size={25} />} text="Dashboard"  />
-            <SidebarItem icon={<BookTypeIcon  color="#1f2937" size={25} />} text="Fichamentos" />
-            <SidebarItem icon={<ClipboardCheckIcon  color="#1f2937" size={25} />} text="Quiz" />
+            <Link to="/" style={{ textDecoration: 'none' }}>
+                <SidebarItem icon={<LayoutDashboard color="#1f2937" size={25} />} text="Dashboard"  />
+            </Link>
+            <Link to="/fichamentos" style={{ textDecoration: 'none' }}>
+                <SidebarItem icon={<BookTypeIcon  color="#1f2937" size={25} />} text="Fichamentos" />
+            </Link>
+            <Link to="/quiz" style={{ textDecoration: 'none' }}>
+                <SidebarItem icon={<ClipboardCheckIcon  color="#1f2937" size={25} />} text="Quiz" />
+            </Link>
         </>
     );
 }
