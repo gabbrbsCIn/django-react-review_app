8bf5d50e6b65c3a8a031250423162a2633229386
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-05-13 17:20:03 -0300

    feat: adicionada lógica para criar um novo fichamento

--- frontend/src/components/AddButton.jsx
+++ frontend/src/components/AddButton.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import { CirclePlus } from 'lucide-react';
+import { useState, useEffect } from 'react';
+import api from '../api';
 
-function AddButton() {
-    const handleAdd = () => {
+function AddButton({ user_id }) {
+    const [revisionTitle, setRevisionTitle] = useState('');
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        setRevisionTitle(e.target.value);
+    }
+
+    const handleAdd = async () => {
+        try {
+            const data = {
+                title: revisionTitle,
+                user_id: user_id
+            };
+
+            const response = await api.post('add-revision', data);
+            console.log('Success:', response.data);
+            alert('Fichamento adicionado com sucesso!');
+        } catch (error) {
+            console.log('Error:', error);
+            alert('Error');
+        }
     };
 
+
     return (
-        <button className='bg-yellow-400 px-4 py-1 rounded-md hover:bg-yellow-500 transition duration-300 ' onClick={handleAdd}><CirclePlus/></button>
+        <div className='flex flex-row items-center'>
+            <input type="text" className='border mx-2 h-full text-sm rounded-md border-2' placeholder='Título do fichamento...' onChange={handleChange} />
+            <button className='bg-yellow-400 px-4 py-1 rounded-md hover:bg-yellow-500 transition duration-300 ' onClick={handleAdd}><CirclePlus /></button>
+        </div>
+        
     );
 }
 
--- frontend/src/pages/Revision.jsx
+++ frontend/src/pages/Revision.jsx
@@ -1,8 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import api from '../api';
+import { ACCESS_TOKEN } from '../constants';
+import AddButton from '../components/AddButton';
 
 
 function Revision() {
+    const route = '/users/get_user/';
+    const [user_id, setUser_id] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const token = localStorage.getItem(ACCESS_TOKEN);
+                if (token) {
+                    const response = await api.get(route);
+                    setUser_id(response.data.user_id);
+                }
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
-        <h1>Revision Page</h1>
+        <>
+            <div className='flex items-center h-full px-36 py-10 justify-center '>
+                <AddButton user_id={user_id} />
+            </div>
+        </>
     )
 
 }
