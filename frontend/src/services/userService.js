5d874bbbe21b60f17be20d30653a6e35659a7b4d
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-05-15 18:25:33 -0300

    feat: adicionado função getUser ao userService

---
+++ frontend/src/services/userService.js
@@ -0,0 +1,21 @@
+import api from "../api";
+import { ACCESS_TOKEN } from "../constants";
+
+const getUser = async (setUser, setIsLoading) => {
+    try {
+      const token = localStorage.getItem(ACCESS_TOKEN);
+      if (token) {
+        const response = await api.get('/users/get_user/');
+        setUser({
+          username: response.data.username,
+          user_id: response.data.user_id,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  
+  export default getUser;
