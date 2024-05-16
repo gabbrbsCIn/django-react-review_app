cdb71c1e0530792de6c445d9459ff03908c50103
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-05-16 16:00:20 -0300

    feat: add lógica para alterar funcionamento do buttoncard

--- frontend/src/components/ButtonCard.jsx
+++ frontend/src/components/ButtonCard.jsx
@@ -1,11 +1,22 @@
+import { useState, useEffect } from "react";
 
+function ButtonCard({ type }) {
+    const [text, setText] = useState('');
+    const [buttonClass, setButtonClass] = useState('bg-yellow-200 px-4 py-1 rounded-xl hover:bg-yellow-400 transition duration-300 mr-2');
 
-function ButtonCard() {
+    useEffect(() => {
+        if (type === "excluir") {
+            setText('Excluir');
+            setButtonClass('bg-red-300 px-4 py-1 rounded-xl hover:bg-red-400 transition duration-300 mr-2');
+        } else if (type === "abrir") {
+            setText('Abrir');
+            setButtonClass('bg-yellow-200 px-4 py-1 rounded-xl hover:bg-yellow-400 transition duration-300 mr-2');
+        }
+    }, [type]);
 
-    return(
-        <button className="bg-yellow-200 px-4 py-1 rounded-xl hover:bg-yellow-400 transition duration-300 mr-2">Abrir</button>
-        )
-
-
+    return (
+        <button className={buttonClass}>{text}</button>
+    )
 }
+
 export default ButtonCard;
