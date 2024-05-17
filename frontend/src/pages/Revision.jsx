915575afdc7aa68de3ab94442921c5fef3892bff
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-05-17 18:18:21 -0300

    refactor: atualizado o componente de revisão para mapear  as revisões da API e exibi-las dinamicamente.

--- frontend/src/pages/Revision.jsx
+++ frontend/src/pages/Revision.jsx
@@ -1,40 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import UserContext from '../contexts/userContext';
 import { useContext } from 'react';
-
+import api from '../services/api';
+import UserContext from '../contexts/userContext';
+import { LoaderIcon } from 'lucide-react';
 import MainLayout from '../components/MainLayout';
 import AddButton from '../components/AddButton';
 import Card from '../components/Card';
 import ButtonCard from '../components/ButtonCard';
 
-
 function Revision() {
     const { user } = useContext(UserContext);
+    const [revisions, setRevisions] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const title = 'Seus Fichamentos';
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await api.get('revision');
+                setRevisions(response.data);
+                setIsLoading(false);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleAddRevision = (newRevision) => {
+        setRevisions([...revisions, newRevision]);
+    };
+
     return (
-
-        <MainLayout title={title}>
-            <div className='flex flex-col'>
-                <div className='flex flex-col pl-36'>
-                    <AddButton user_id={user.user_id} />
-                </div>
-                <div className='flex flex-wrap py-8 ml-36'>
-                    <Card text={"Fichamento de Matemática"}>
-                        <ButtonCard type={"abrir"} />
-                    </Card>
-                    <Card text={"Fichamento de Matemática"}>
-                        <ButtonCard type={"excluir"} />
-                        <ButtonCard type={"abrir"} />
-
-                    </Card>
-
-                </div>
-            </div>
-        </MainLayout>
-
-    )
-
+        <>
+            {isLoading ? <div className="flex w-screen justify-center items-center animate-spin"><LoaderIcon size={30} /></div> :
+                <MainLayout title={title}>
+                    <div className='flex flex-col'>
+                        <div className='flex flex-col pl-36'>
+                            <AddButton onAdd={handleAddRevision} />
+                        </div>
+                        <div className='flex flex-wrap py-8 ml-36'>
+                            {revisions.map((revision) => (
+                                <Card text={revision.title} key={revision.id} {...revision}>
+                                    <ButtonCard type={"abrir"} />
+                                    <ButtonCard type={"excluir"} />
+                                </Card>
+                            ))}
+                        </div>
+                    </div>
+                </MainLayout>}
+        </>
+    );
 }
 
 export default Revision;
