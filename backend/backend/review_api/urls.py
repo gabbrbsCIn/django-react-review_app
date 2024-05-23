2df09a6e80ce578e2bc1a20cead6a3a52e780071
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-05-23 15:24:43 -0300

    refactor: Atualizado URLs do módulo review_api

--- backend/backend/review_api/urls.py
+++ backend/backend/review_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GetReview, SaveQuiz, RevisionManager
+from .views import GetReview, SaveQuiz, RevisionManagerView, RevisionDestroyView
 from . import views
 
 urlpatterns = [
     path("get-review", GetReview.as_view(), name="get_review"),
     path("save-quiz", SaveQuiz.as_view(), name="save_quiz"),
-    path("revision", RevisionManager.as_view(), name="revision"),
+    path("revision", RevisionManagerView.as_view(), name="revision"),
+    path("revision/<int:pk>", RevisionDestroyView.as_view(), name="delete_revision"),
 ]
