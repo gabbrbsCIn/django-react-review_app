f9a9477667440febc59757e11f81e8a85ab15973
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-05-24 11:48:22 -0300

    refactor: atualizado componente TextBox para incluir o título do fichamento na renderização.

--- frontend/src/components/TextBox.jsx
+++ frontend/src/components/TextBox.jsx
@@ -3,7 +3,7 @@ import api from '../services/api';
 
 import ReviewButton from './ReviewButton';
 
-function TextBox() {
+function TextBox({ revision }) {
 
     const [text, setText] = useState("")
     const [quiz, setQuiz] = useState([])
@@ -11,7 +11,7 @@ function TextBox() {
     const handleChange = (event) => {
         setText(event.target.value);
     }
-
+    console.log(revision.title)
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -24,24 +24,23 @@ function TextBox() {
         } catch (error) {
             console.error(error);
         }
+
+
     }
 
     return (
-        <>
-            <div className='flex justify-center h-screen w-screen py-20 '>
-                <div className='flex flex-col items-center w-2/3 bg-gray-200 p-4 rounded-lg font-poppins '>
-                    <h1 className='mb-2'>Fichamento</h1>
-                    <textarea
-                        onChange={handleChange}
-                        value={text}
-                        className='bg-gray-100 w-full p-2 border border-gray-300 rounded-lg focus:outline-none h-full max-h-full'
-                        placeholder="Digite aqui..."
-                    />
-                    <ReviewButton route="/get-review" data={text} />
-                </div>
-
+        <div className='flex justify-center h-screen w-full py-20 px-36'>
+            <div className='flex flex-col items-center w-full bg-gray-200 p-4 rounded-lg font-poppins'>
+                <h1 className='mb-2'>{revision.title}</h1>
+                <textarea
+                    onChange={handleChange}
+                    value={text}
+                    className='bg-gray-100 w-full p-2 border border-gray-300 rounded-lg focus:outline-none h-full max-h-full'
+                    placeholder="Digite aqui..."
+                />
+                <ReviewButton route="/get-review" data={text} />
             </div>
-        </>
+        </div>
     )
 }
 
