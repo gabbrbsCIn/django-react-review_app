5f5915319ad27213b3eb7bffa43f1351f0d26117
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-06-03 13:52:58 -0300

    refactor: atualizado componente Card para usar 'title' prop no lugar de 'text' prop

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,7 +1,7 @@
-function Card({ children, text }) {
+function Card({ children, title }) {
     return (
         <div className="group flex flex-col items-center justify-between bg-white shadow-lg rounded-lg w-72 h-48 mr-16 my-5 relative transition-transform transform-gpu hover:translate-y-[-4px] hover:shadow-xl">
-            <h1 className="text-lg py-8 flex items-center justify-center h-full">{text}</h1>
+            <h1 className="text-lg py-8 flex items-center justify-center h-full">{title}</h1>
             <div className="absolute bottom-0 left-0 w-full h-10 flex items-center justify-end">
                 {children}
             </div>
