267cc15255208c1f7d4f9d5d3044c0be5f974753
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-06-04 12:03:25 -0300

    refactor: Add QuizSerializer ao serializers.py

--- backend/backend/review_api/serializers.py
+++ backend/backend/review_api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Revision
+from .models import Revision, Quiz 
 
 class RevisionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Revision
         fields = ['id', 'title', 'text', 'user_id']
         read_only_fields = ['user_id']
+
+class QuizSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = ['id', 'title', 'description', 'revision_id']
+        read_only_fields = ['revision_id']
