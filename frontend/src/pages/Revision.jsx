2f13fa969c3d0a0aeaedccb2f5fad62cba43cd98
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-06-04 12:08:42 -0300

    refactor: atualizado componente ButtonCard e Revision para incluir propriedade correta da chamada à API

--- frontend/src/pages/Revision.jsx
+++ frontend/src/pages/Revision.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useContext } from 'react';
-import api from '../services/api';
-import UserContext from '../contexts/userContext';
+import React, { useContext } from 'react';
+import RevisionContext from '../contexts/revisionContext';
 import { LoaderIcon } from 'lucide-react';
 import MainLayout from '../components/MainLayout';
 import AddButton from '../components/AddButton';
@@ -9,33 +7,8 @@ import Card from '../components/Card';
 import ButtonCard from '../components/ButtonCard';
 
 function Revision() {
-    const [revisions, setRevisions] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+    const { revisions, isLoading, addRevision, removeRevision } = useContext(RevisionContext);
     const title = 'Seus Fichamentos';
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await api.get('revision');
-                console.log(response.data);
-                setRevisions(response.data);
-                console.log(revisions);
-                setIsLoading(false);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    const handleAddRevision = (newRevision) => {
-        setRevisions([...revisions, newRevision]);
-    };
-
-    const handleRemoveRevision = (id) => {
-        setRevisions(revisions.filter(revision => revision.id !== id));
-    };
 
     return (
         <>
@@ -43,14 +16,14 @@ function Revision() {
                 <MainLayout title={title}>
                     <div className='flex flex-col'>
                         <div className='flex flex-col pl-36'>
-                            <AddButton onAdd={handleAddRevision} />
+                            <AddButton onAdd={addRevision} />
                         </div>
                         <div className='flex flex-wrap py-8 ml-36 overflow-y-auto max-h-[calc(100vh-9rem)]'>
                             {revisions.length === 0 ? <h1 className='text-xl w-full'>Nenhum fichamento encontrado </h1> :
                                 revisions.map((revision) => (
                                     <Card title={revision} key={revision.id} {...revision}>
                                         <ButtonCard type={"abrir"} id={revision.id} />
-                                        <ButtonCard type={"excluir"} id={revision.id} onRemove={handleRemoveRevision} />
+                                        <ButtonCard type={"excluir"} id={revision.id} onRemove={removeRevision} route={"revision"} />
                                     </Card>
                                 ))}
                         </div>
