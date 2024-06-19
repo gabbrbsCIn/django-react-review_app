3110e3fcb95eabedeb93ab43300da31cb9c2e598
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-06-19 17:21:21 -0300

    refactor: Atualizado componente Form como nova estilização e lógica para incluir Linki de cadastro e login

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../services/constants";
 import "../index.css";
 import api from "../services/api";
@@ -13,6 +13,7 @@ function Form({ route, method }) {
     const navigate = useNavigate();
 
     const name = method === "login" ? "Login" : "Register";
+    const linkRoute = method === "login" ? "/register" : "/login";
 
     const handleSubmit = async (e) => {
         setLoading(true)
@@ -45,25 +46,31 @@ function Form({ route, method }) {
             <div className="relative w-screen h-screen font-poppins bg-amber-400 flex items-center justify-center">
                 <div className="absolute inset-0 z-0 " style={{ backgroundImage: "url('/2174692.svg')", backgroundSize: "cover", opacity: "0.1" }}></div>
 
-                <div className="bg-amber-200 py-10 px-6 rounded-2xl shadow-2xl z-10">
+                <div className="bg-white py-10 px-6 rounded-2xl shadow-2xl z-10">
                     <form onSubmit={handleSubmit} className="flex flex-col items-center space-y-11 mb-5 pr-4 pl-4">
                         <h1 className="text-3xl"> {name} </h1>
                         <input
-                            className="appearance-none border border-gray-300 rounded-2xl w-full py-2 px-3 mb-3 mr-4 ml-4 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300 ease-in-out"
+                            className="appearance-none border bg-gray-300 rounded-2xl w-full py-2 px-3 mb-3 mr-4 ml-4 focus:outline-none focus:ring-2 focus:ring-gray-500 transition-all duration-300 ease-in-out"
                             type="text"
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
                             placeholder="Nome de Usuário"
                         />
                         <input
-                            className="appearance-none border border-gray-300 rounded-2xl w-full py-2 px-3 mb-3 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300 ease-in-out"
+                            className="appearance-none border bg-gray-300 rounded-2xl w-full py-2 px-3 mb-3 focus:outline-none focus:ring-2 focus:ring-gray-500 transition-all duration-300 ease-in-out"
                             type="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             placeholder="Senha"
                         />
-                        <button className="bg-blue-600 hover:bg-blue-800 text-white py-2 px-6 rounded-full shadow-lg transition-all duration-300 ease-in-out" type="submit"> {name} </button>
+                        <button className="bg-amber-400 hover:bg-amber-500 text-white py-2 px-6 rounded-full shadow-lg transition-all duration-300 ease-in-out" type="submit"> {name} </button>
                     </form>
+                    <div className="text-center text-xs mt-10">
+                        <p>
+                            {method === "login" ? "Não tem uma conta?" : "Já tem uma conta?"}
+                            <Link to={linkRoute} className="text-blue-500 underline ml-1">{name}</Link>
+                        </p>
+                    </div>
                 </div>
             </div>
 
